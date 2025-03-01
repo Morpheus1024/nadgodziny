fb08e05a506e327f13dcf99b26a12f94baa4e769
Mikołaj Galant <mikolaj.galant@gmail.com>
2025-03-01 14:04:10 +0100

    Fixes

--- kalkulator_nadgodzin.py
+++ kalkulator_nadgodzin.py
@@ -19,7 +19,7 @@ def dodaj_czas_do_jsona(czas):
 
 def odejmij_czas_z_jsona(czas): 
     
-    godziny, minuty = czas.split(",")
+    godziny, minuty = czas.split(".")
     czas = (60 * int(godziny) + int(minuty))
     
     with open("nadgodziny.json", "r") as f:
@@ -41,14 +41,14 @@ def odejmij_czas_z_jsona(czas):
 
 
 def oblicz_nadgodziny():
-    czas_rozpoczecia = input("Podaj czas rozpoczecia: ")
-    czas_zakonczenia = input("Podaj czas zakonczenia: ")
+    czas_rozpoczecia = input("Podaj czas rozpoczecia [gg.mm]: ")
+    czas_zakonczenia = input("Podaj czas zakonczenia [gg.mm]: ")
 
-    godzina_ropoczecia, minuta_rozpoczecia = czas_rozpoczecia.split(",")
-    godzina_zakonczenia, minuta_zakonczenia = czas_zakonczenia.split(",")
+    godzina_ropoczecia, minuta_rozpoczecia = czas_rozpoczecia.split(".")
+    godzina_zakonczenia, minuta_zakonczenia = czas_zakonczenia.split(".")
 
-    print(godzina_ropoczecia, minuta_rozpoczecia)
-    print(godzina_zakonczenia, minuta_zakonczenia)
+    print(f"Rozpoczęto o {godzina_ropoczecia}:{minuta_rozpoczecia}")
+    print(f"Skończono o  {godzina_zakonczenia}:{minuta_zakonczenia}")
 
     czas = (60 * int(godzina_zakonczenia) + int(minuta_zakonczenia)) - (60 * int(godzina_ropoczecia) + int(minuta_rozpoczecia)) - 8*60
 
@@ -59,22 +59,22 @@ def oblicz_nadgodziny():
         print("Nadal nic nie robie")
     else: dodaj_czas_do_jsona(czas)
 
-# if __name__ == "__main__":
-with open("nadgodziny.json", "r") as f:
-            json_data = json.load(f)
-#print(json_data["godziny"], "h", json_data["minuty"], "min")
-print("1. Policz nadgodziny")
-print("2. Odczyt nadgodzin z pliku")
-print("3. Odejmij nadgodziny")
-
-wybor = input("Wybierz opcję: ")
-
-
-if wybor == "1":
-    oblicz_nadgodziny()
-elif wybor == "2":
-    print()
-    print(json_data["godziny"], "h", json_data["minuty"], "min")
-elif wybor == "3":
-    czas = input("Podaj czas do odjęcia: ")
-    odejmij_czas_z_jsona(czas)
+if __name__ == "__main__":
+    with open("nadgodziny.json", "r") as f:
+                json_data = json.load(f)
+    #print(json_data["godziny"], "h", json_data["minuty"], "min")
+    print("1. Policz nadgodziny")
+    print("2. Odczyt nadgodzin z pliku")
+    print("3. Odejmij nadgodziny")
+    
+    wybor = input("Wybierz opcję: ")
+    
+    
+    if wybor == "1":
+        oblicz_nadgodziny()
+    elif wybor == "2":
+        print()
+        print(json_data["godziny"], "h", json_data["minuty"], "min")
+    elif wybor == "3":
+        czas = input("Podaj czas do odjęcia: ")
+        odejmij_czas_z_jsona(czas)
